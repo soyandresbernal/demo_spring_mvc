e2ea21e2d73c2ae5c38f965b4fd1f032a1b5b64a
serious <soyandresbernal@gmail.com>
2025-03-24 15:38:46 -0500

    feat: :art: best controller
    controller with thymeleaf OK

--- src/main/java/com/springboot/data/springdemo/controllers/UserController.java
+++ src/main/java/com/springboot/data/springdemo/controllers/UserController.java
@@ -1,12 +1,16 @@
 package com.springboot.data.springdemo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
     @GetMapping("/details")
-    public String details() {
+    public String details(Model model) {
+        model.addAttribute("title", "Hi springboot");
+        model.addAttribute("name", "andrés");
+        model.addAttribute("lastname", "serious");
         return "details";
     }
 }
