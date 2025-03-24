4cb8f0ec0276af5f1beca566d000c2cad31fad60
serious <soyandresbernal@gmail.com>
2025-03-24 13:43:31 -0500

    feat: :art: Inicializar la estructura base del proyecto Java MVC funcional
    Inicializar la estructura base del proyecto Java MVC funcional
    
    controller OK

---
+++ src/main/java/com/springboot/data/springdemo/controllers/UserController.java
@@ -0,0 +1,12 @@
+package com.springboot.data.springdemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/details")
+    public String details() {
+        return "details";
+    }
+}
