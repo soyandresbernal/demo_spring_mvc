cf944f8d4037b61f416097c3c8dda710d1bf9d80
serious <soyandresbernal@gmail.com>
2025-03-25 15:41:47 -0500

    feat: :bug: create list  in Resfull
    list and restfull
    
    list and restfull

--- src/main/java/com/springboot/data/springdemo/controllers/UserRestController.java
+++ src/main/java/com/springboot/data/springdemo/controllers/UserRestController.java
@@ -1,7 +1,11 @@
 package com.springboot.data.springdemo.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.data.springdemo.models.User;
@@ -19,5 +23,20 @@ public class UserRestController {
         userDto.setUser(user);
         userDto.setTitle("Hi Spring");
         return userDto;
+    }
+
+    @GetMapping("/list")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+
+    public List<User> list() {
+        User user = new User("Andrés", "Serious");
+        User user2 = new User("Juan", "Serious");
+        User user3 = new User("Dalo", "Serious");
+
+        List<User> users = Arrays.asList(user, user2, user3);
+
+        return users;
     }
 }
