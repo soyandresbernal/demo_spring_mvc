91a83203180178321f8bf3eafc8a87af1544f287
serious <soyandresbernal@gmail.com>
2025-03-25 15:21:31 -0500

    feat: add Dto port:8081
    dto
    
    dto

--- src/main/java/com/springboot/data/springdemo/controllers/UserRestController.java
+++ src/main/java/com/springboot/data/springdemo/controllers/UserRestController.java
@@ -1,22 +1,23 @@
 package com.springboot.data.springdemo.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.data.springdemo.models.User;
+import com.springboot.data.springdemo.models.dto.UserDto;
+
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
-    @GetMapping("/details")
-    public Map<String, Object> details() {
-        Map<String, Object> body = new HashMap<>();
 
-        body.put("title", "Hi spring boot");
-        body.put("name", "Andrés");
-        body.put("lastname", "serious");
-        return body;
+    @GetMapping("/details")
+    public UserDto details() {
+
+        UserDto userDto = new UserDto();
+        User user = new User("Andrés", "Serious");
+        userDto.setUser(user);
+        userDto.setTitle("Hi Spring");
+        return userDto;
     }
 }
---
+++ src/main/java/com/springboot/data/springdemo/models/User.java
@@ -0,0 +1,31 @@
+package com.springboot.data.springdemo.models;
+
+public class User {
+    private String name;
+    private String lastname;
+
+    public User() {
+    }
+
+    public User(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+}
