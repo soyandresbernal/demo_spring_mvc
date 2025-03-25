fd028fc067bec7fbb1d812c62a32425635061352
serious <soyandresbernal@gmail.com>
2025-03-25 14:38:01 -0500

    feat: :bug: fix restController
    Rest controller
    
    rest controller

--- src/main/java/com/springboot/data/springdemo/controllers/UserRestController.java
+++ src/main/java/com/springboot/data/springdemo/controllers/UserRestController.java
@@ -4,11 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
-    @GetMapping("/details2")
+    @GetMapping("/details")
     public Map<String, Object> details() {
         Map<String, Object> body = new HashMap<>();
 
